ccf23f01ad2dfbea12db277c1af3860b420d9f11
Pippijn van Steenhoven <pip88nl@gmail.com>
2010-03-01 04:30:27 +0100

    shut down correctly and set program name

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,8 +5,17 @@ extern char** environ;
 int
 main (int argc, char* argv[])
 {
+  char const* prgname = strrchr (argv[0], '/');
+  if (prgname)
+    ++prgname;
+  else
+    prgname = argv[0];
+  g_set_prgname (prgname);
+
   init_server (argc, argv, environ);
   server->run ();
+  uninit_server ();
+  puts ("done");
 
   return 0;
 }
