284a1dadf3225de2bc312fd18bfbddd654c96375
Moritz Wilhelmy <n0nsense@xinutec.org>
2010-03-02 00:38:54 +0100

    moved numerics in c.cpp and implemented conversion

--- src/PurpleBee/marshal/c.cpp
+++ src/PurpleBee/marshal/c.cpp
@@ -1,22 +1,6 @@
 #include "perl/interpreter.h"
 #include "util/unimplemented.h"
 
-#if IVSIZE >= 8
-typedef IV val64;
-typedef IV uval64;
-# define newSVval64 newSViv
-# define SvVAL64 SvIV
-# define newSVuval64 newSVuv
-# define SvUVAL64 SvUV
-#else
-typedef double val64;
-typedef double uval64;
-# define newSVval64 newSVnv
-# define SvVAL64 SvNV
-# define newSVuval64 newSVnv
-# define SvUVAL64 SvNV
-#endif
-
 /*
  * T -> SV*
  */
@@ -58,48 +42,6 @@ perl_interpreter::to_sv (int* v)
 
 template<>
 SV*
-perl_interpreter::to_sv (int v)
-{
-  return newSViv (v);
-}
-
-template<>
-SV*
-perl_interpreter::to_sv (long long v)
-{
-  return newSVval64 (v);
-}
-
-template<>
-SV*
-perl_interpreter::to_sv (unsigned long long v)
-{
-  return newSVuval64 (v);
-}
-
-template<>
-SV*
-perl_interpreter::to_sv (unsigned long v)
-{
-  return newSVuv (v);
-}
-
-template<>
-SV*
-perl_interpreter::to_sv (unsigned int v)
-{
-  return newSVuv (v);
-}
-
-template<>
-SV*
-perl_interpreter::to_sv (long v)
-{
-  return newSViv (v);
-}
-
-template<>
-SV*
 perl_interpreter::to_sv (char const* v)
 {
   STRLEN len = 0;
@@ -134,59 +76,9 @@ perl_interpreter::to_sv (unsigned char** v)
   UNIMPLEMENTED;
 }
 
-template<>
-SV*
-perl_interpreter::to_sv (double v)
-{
-  UNIMPLEMENTED;
-}
-
-
 /*
  * SV* -> T
  */
-
-template<>
-int
-perl_interpreter::sv_to (SV* v)
-{
-  return SvIV (v);
-}
-
-template<>
-unsigned int
-perl_interpreter::sv_to (SV* v)
-{
-  return SvUV (v);
-}
-
-template<>
-unsigned long
-perl_interpreter::sv_to (SV* v)
-{
-  return SvUV (v);
-}
-
-template<>
-long long
-perl_interpreter::sv_to (SV* v)
-{
-  return SvVAL64 (v);
-}
-
-template<>
-unsigned long long
-perl_interpreter::sv_to (SV* v)
-{
-  return SvUVAL64 (v);
-}
-
-template<>
-long
-perl_interpreter::sv_to (SV* v)
-{
-  return SvIV (v);
-}
 
 template<>
 char const*
