33f4f78dbf7c44485e7294b7dadcae32e718f41e
Pippijn van Steenhoven <pip88nl@gmail.com>
2011-06-08 12:06:35 +0200

    use autoproj; fix distcheck; rename util to pbutil

--- src/common/util/xassert.cpp
+++ src/common/util/xassert.cpp
@@ -6,7 +6,7 @@
 #include "common/util/xassert.h"
 
 std::string
-assertion_failure::construct (char const* msg, char const* file, int line, char const* func)
+assertion_failure::construct (char const *msg, char const *file, int line, char const *func)
 {
   std::ostringstream s;
 
